1a87d1783770d6836ba25fc5753f43436df38b3c
dcweber <dcweber@us.ibm.com>
2020-04-03 08:18:41 -0500

    update version import in common.py

--- ibm_whcs_sdk/common.py
+++ ibm_whcs_sdk/common.py
@@ -19,7 +19,7 @@ This module provides common methods for use across all service modules.
 """
 
 import platform
-from version import __version__
+from .version import __version__
 
 HEADER_NAME_USER_AGENT = 'User-Agent'
 SDK_NAME = 'ibm-iae-python-sdk'
