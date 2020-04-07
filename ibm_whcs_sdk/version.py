778320254d78c4dfda6c981604485f4ea69d723d
Anthony W. Eichenlaub <eichenla@us.ibm.com>
2020-04-07 13:51:23 +0000

    Update version 0.0.7 -> 0.0.8

--- ibm_whcs_sdk/version.py
+++ ibm_whcs_sdk/version.py
@@ -1,4 +1,4 @@
 """
 SDK version
 """
-__version__ = '0.0.7'
+__version__ = '0.0.8'
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ import os
 import sys
 import pkg_resources
 
-__version__ = '0.0.7'
+__version__ = '0.0.8'
 PACKAGE_NAME = 'ibm_whcs_sdk'
 
 #with open('requirements.txt') as f:
