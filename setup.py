d0f30ad6fbcc32ee89a2ffe58750cb7d0d1e4d08
Watson Health Build <whcsbld@us.ibm.com>
2020-10-06 22:16:35 -0500

    Update version 1.7.0 -> 1.8.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ import os
 import sys
 import pkg_resources
 
-__version__ = '1.7.0'
+__version__ = '1.8.0'
 PACKAGE_NAME = 'ibm_whcs_sdk'
 PACKAGE_DESC = 'This is the Watson Health Cognitive Services Python SDK containing ACD and IML',
 
