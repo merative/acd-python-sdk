7957a948f065ad3d65a929b92899c46a6f1dcf06
Deb Angst <dlangst@us.ibm.com>
2020-11-02 12:21:37 -0600

    WHS-68 #comment support temporal feature

--- ibm_whcs_sdk/annotator_for_clinical_data/tests/integration/model/test_acd_temporal_span_model.py
+++ ibm_whcs_sdk/annotator_for_clinical_data/tests/integration/model/test_acd_temporal_span_model.py
@@ -18,6 +18,6 @@ import ibm_whcs_sdk.annotator_for_clinical_data as wh
 
 def test_TemporalSpan_model():
     temporal_type_dict = {}
-    model = wh.TemporalSpan(begin=1, end=2, covered_text="We got you covered",
+    model = wh.Temporal(begin=1, end=2, covered_text="We got you covered",
                            temporal_type=temporal_type_dict, extra="more")
     assert model.__str__() is not None
